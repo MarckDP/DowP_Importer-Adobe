3390d08c8b8f7006cd8eac10b4398f923d42191a
MarckDP <130426515+MarckDP@users.noreply.github.com>
2025-08-25 15:38:47 -0500

    Update run_dowp.bat

--- DowP_EXT/run_dowp.bat
+++ DowP_EXT/run_dowp.bat
@@ -1,7 +1,7 @@
 @echo off
-REM 
+REM Cambia al directorio donde se encuentra este script .bat
 cd /d "%~dp0"
 
-REM 
+REM Lanza el script de Python sin consola, pasándole cualquier argumento que reciba este .bat
 echo Lanzando DowP...
 pythonw "main.py" %1
