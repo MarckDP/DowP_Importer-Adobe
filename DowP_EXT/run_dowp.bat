04744867a0b91087400317da5d062ba261d66040
MarckDP <130426515+MarckDP@users.noreply.github.com>
2025-08-28 08:36:46 -0500

    Delete DowP_EXT directory

--- DowP_EXT/run_dowp.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Cambia al directorio donde se encuentra este script .bat
-cd /d "%~dp0"
-
-REM Lanza el script de Python sin consola, pasándole cualquier argumento que reciba este .bat
-echo Lanzando DowP...
-pythonw "main.py" %1
--- DowP_EXT/src/core/exceptions.py
+++
@@ -1,8 +0,0 @@
-class UserCancelledError(Exception):
-    """Excepción lanzada cuando el usuario cancela una operación."""
-    pass
-class LocalRecodeFailedError(Exception):
-    """Excepción para un fallo específico en la recodificación local."""
-    def __init__(self, message, temp_filepath=None):
-        super().__init__(message)
-        self.temp_filepath = temp_filepath
